ad4c92ecb1293212814c89dbf76734a6dfb035db
Furkan <furkan.ayag@solidict.com>
2025-01-24 10:51:40 +0300

    first

---
+++ src/main/java/com/yerinden/yerinden/YerindenApplication.java
@@ -0,0 +1,13 @@
+package com.yerinden.yerinden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YerindenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YerindenApplication.class, args);
+	}
+
+}
